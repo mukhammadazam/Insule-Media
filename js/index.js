7401b7f9d1001b9b58ab8c4ca3c67ac926441738
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-02-07 08:54:33 +0300

    index.js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let drabdawn = document.querySelector(".drabdawn");
+let list11 = document.querySelector('.list11');
+let list12 = document.querySelector('.list12');
+drabdawn.addEventListener('click', function () {
+    list11.classList.toggle("list12");
+});
