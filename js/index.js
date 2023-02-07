462a945bd8814acc09ce0f879626f07320304264
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-02-07 09:20:20 +0300

    dostavka text start

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 let drabdawn = document.querySelector(".drabdawn");
 let list11 = document.querySelector('.list11');
 let list12 = document.querySelector('.list12');
+let list13 = document.querySelector(".list13");
+let list14 = document.querySelector(".list14");
 drabdawn.addEventListener('click', function () {
     list11.classList.toggle("list12");
+    list13.classList.toggle("list14");
 });
