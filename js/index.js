ae02e79ea08def8d21768bd8475e2720b85fa8da
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-02-11 15:47:49 +0300

    initial commit

--- js/index.js
+++ js/index.js
@@ -3,7 +3,14 @@ let list11 = document.querySelector('.list11');
 let list12 = document.querySelector('.list12');
 let list13 = document.querySelector(".list13");
 let list14 = document.querySelector(".list14");
+let pptext = document.querySelector('.pptext');
+let text12 = document.querySelector('.text12');
 drabdawn.addEventListener('click', function () {
     list11.classList.toggle("list12");
     list13.classList.toggle("list14");
+
 });
+text12.addEventListener('click', function () {
+    pptext.classList.toggle('pptext1')
+})
+
